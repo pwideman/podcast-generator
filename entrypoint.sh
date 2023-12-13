fa1efc51a038c4df19db653e721abdf186317c76
Paul Wideman <pwideman@users.noreply.github.com>
2023-12-13 20:33:55 +0000

    Skip commit and push if no changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,10 @@ git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
+if [[ -z $(git status -s) ]]; then
+  echo "No changes to commit"
+  exit 0
+fi
 git add -A && git commit -m "Update feed"
 git push --set-upstream origin main
 
